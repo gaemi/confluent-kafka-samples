c2623afdbb96a5245a1e94e325342adf766a366d
JungHoon Song <segaji@gmail.com>
2018-07-21 22:51:18 +0900

    Spring Kafka 관련 예제코드 수정

--- spring-kafka/producer/src/main/java/io/github/gaemi/producer/SpringKafkaProducerApplication.java
+++ spring-kafka/producer/src/main/java/io/github/gaemi/producer/SpringKafkaProducerApplication.java
@@ -1,10 +1,8 @@
 package io.github.gaemi.producer;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@EnableAutoConfiguration
 @SpringBootApplication
 public class SpringKafkaProducerApplication {
 
